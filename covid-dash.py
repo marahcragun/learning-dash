32f9f02edc4927d005fedada6813b6c938ba73e6
Ander Fuller <anderfuller@gmail.com>
2024-03-27 15:18:12 -0600

    i am right

--- covid-dash.py
+++ covid-dash.py
@@ -4,5 +4,6 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-
-app = dash.Dash(__name__) #starts the app
+#NO
+#app = dash.Dash(__name__) #starts the app
+#ur wrong
